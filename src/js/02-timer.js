d214047a2a2d8c156b4014559b8bc4fda33d36d6
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-23 19:18:55 +0300

    hw09js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,13 @@
 import flatpickr from "flatpickr";
+import convertMs from './dateConvert';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import "flatpickr/dist/flatpickr.min.css";
+
+let getRef = selector => document.querySelector(selector);
+const imputDatePickerRef = getRef('#datetime-picker');
+const btnStartRef = getRef('[data-start]');
+const daysRef = getRef('[data-days]');
+const hoursRef = getRef('[data-hours]');
+const minutesRef = getRef('[data-minutes]');
+const secondsRef = getRef('[data-seconds]');
