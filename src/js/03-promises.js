4edae486c92301d6869119c503123601cf66124f
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-25 16:19:22 +0300

    hw09js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -27,10 +27,9 @@ function createPromise(position, delay) {
 
 function onPromiseCreate(e) {
   e.preventDefault();
-  const { delay, step, amount } = e.currentTarget.elements;
-  let inputDelay = Number(delay.value);
-  let inputStep = Number(step.value);
-  let inputAmount = Number(amount.value);
+  let valueDelay = Number(refs.delay.value);
+  let step = Number(refs.step.value);
+  let amount = Number(refs.amount.value);
 
   for (let i = 1; i <= inputAmount; i += 1) {
     inputDelay += inputStep;
